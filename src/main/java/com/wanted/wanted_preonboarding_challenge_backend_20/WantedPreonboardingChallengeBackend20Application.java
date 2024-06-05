876e68b7576567cdc88b15d2951d9ae3458857c1
dongha-byun <byunsw4@naver.com>
2024-06-05 11:46:03 +0900

    init commit

---
+++ src/main/java/com/wanted/wanted_preonboarding_challenge_backend_20/WantedPreonboardingChallengeBackend20Application.java
@@ -0,0 +1,13 @@
+package com.wanted.wanted_preonboarding_challenge_backend_20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WantedPreonboardingChallengeBackend20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WantedPreonboardingChallengeBackend20Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wanted/wanted_preonboarding_challenge_backend_20/WantedPreonboardingChallengeBackend20ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanted.wanted_preonboarding_challenge_backend_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WantedPreonboardingChallengeBackend20ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
